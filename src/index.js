af94b91d9846536e01302be66f4d62769caffa0d
Yauheniya Viarbitskaya <evgeniya.verbiczkaya98@gmail.com>
2018-09-23 20:04:32 +0300

    d

--- src/index.js
+++ src/index.js
@@ -1,31 +1,46 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
 
-  add(element) {
-    // your implementation
-  }
+    constructor() {
+        this.arr = new Array();
+        this.elements_counter = 0;
+        this.comparator = (left, right) => left - right;
+    }
 
-  at(index) {
-    // your implementation
-  }
+    add(element) {
+        this.arr.push(element);
+        this.elements_counter++;
+    }
 
-  get length() {
-    // your implementation
-  }
+    at(index) {
+        return this.arr[index];
+    }
 
-  toArray() {
-    // your implementation
-  }
+    get length() {
+        return this.elements_counter;
+    }
 
-  sort(indices) {
-    // your implementation
-  }
+    toArray() {
+        return this.arr;
+    }
 
-  setComparator(compareFunction) {
-    // your implementation
-  }
+    sort(indices) {
+        let new_arr = new Array();
+
+        for (let i = 0; i < indices.length; i++) {
+            new_arr.push(this.arr[indices[i]]);
+        }
+
+        new_arr.sort(this.comparator);
+        indices.sort((a, b) => a - b);
+
+        for (let j = 0; j < new_arr.length; j++) {
+            this.arr[indices[j]] = new_arr[j];
+        }
+    }
+
+    setComparator(compareFunction) {
+        this.comparator = compareFunction;
+    }
 }
 
 module.exports = Sorter;
